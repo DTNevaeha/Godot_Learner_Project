f93206e4a525da9083e2697c3e105dce151ff721
DTNevaeha <DTNevaeha@gmail.com>
2023-12-11 12:11:59 -0600

    Created Inheritence scenes

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,12 +1,8 @@
 extends Node2D
+class_name LevelParent
 
 var grenade_scene: PackedScene = preload("res://scenes/projectiles/grenade.tscn")
 var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
-
-
-func _on_gate_player_entered_gate(_body):
-	var tween = create_tween()
-	tween.tween_property($Player, "speed", 0, 0.5)
 
 
 func _on_player_laser(pos, direction):
@@ -22,17 +18,6 @@ func _on_player_grenade(pos, direction):
 	grenade.position = pos
 	grenade.linear_velocity = direction * grenade.speed
 	$Projectiles.add_child(grenade)
-
-
-# func _process(_delta):
-# 	var max_distance = 500
-# 	# If projectiles have moved further than the max distance then they are removed from the list
-# 	for projectile in $Projectiles.get_children():
-# 		if (
-# 			projectile.has_method("has_traveled_too_far")
-# 			and projectile.has_traveled_too_far(max_distance)
-# 		):
-# 			projectile.queue_free()
 
 
 func _on_house_player_entered():
