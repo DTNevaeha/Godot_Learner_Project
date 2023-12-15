97b7ed0a7822b58cfc828b3ddc3c7db4f9c857b8
DTNevaeha <DTNevaeha@gmail.com>
2023-12-15 17:29:37 -0600

    Added level transition

--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -4,3 +4,5 @@ extends LevelParent
 func _on_gate_player_entered_gate(_body):
 	var tween = create_tween()
 	tween.tween_property($Player, "speed", 0, 0.5)
+	# Transition to next level
+	get_tree().change_scene_to_file("res://scenes/levels/inside.tscn")
