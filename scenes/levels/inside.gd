efd2f9f236c352729ef4962943d1c6c29bb881a3
DTNevaeha <DTNevaeha@gmail.com>
2023-12-17 13:13:00 -0600

    Added level transition

--- scenes/levels/inside.gd
+++ scenes/levels/inside.gd
@@ -1,10 +1,8 @@
 extends LevelParent
 
-@export var outside_level_scene: PackedScene
 
-func _on_exit_gate_area_body_entered(_body:Node2D):
-    var tween = create_tween()
-    tween.tween_property($Player, "speed", 0, 0.5)
-    # Transition to next level
-    # get_tree().change_scene_to_file("res://scenes/levels/outside.tscn")
-    get_tree().change_scene_to_packed(outside_level_scene)
+func _on_exit_gate_area_body_entered(_body: Node2D):
+	var tween = create_tween()
+	tween.tween_property($Player, "speed", 0, 0.5)
+	# Transition to next level
+	TransitionLayer.change_scene("res://scenes/levels/outside.tscn")
