868389254128810320cd1789210d3498ecc0c5c8
DTNevaeha <DTNevaeha@gmail.com>
2023-12-19 17:07:42 -0600

    Added setters

--- scenes/items/item.gd
+++ scenes/items/item.gd
@@ -6,18 +6,25 @@ var type = avaliable_options[randi() % len(avaliable_options)]
 
 
 func _ready():
-    if type == "laser":
-        $Sprite2D.modulate = Color(0.1, 0.2, 0.8)
-    elif type == "grenade":
-        $Sprite2D.modulate = Color(0.8, 0.2, 0.1)
-    elif type == "health":
-        $Sprite2D.modulate = Color(0.1, 0.8, 0.1)
+	if type == "laser":
+		$Sprite2D.modulate = Color(0.1, 0.2, 0.8)
+	elif type == "grenade":
+		$Sprite2D.modulate = Color(0.8, 0.2, 0.1)
+	elif type == "health":
+		$Sprite2D.modulate = Color(0.1, 0.8, 0.1)
 
 
 func _process(delta):
-    rotation += rotation_speed * delta
+	rotation += rotation_speed * delta
 
 
-func _on_body_entered(body:Node2D):
-    body.add_item(type)
-    queue_free()
+func _on_body_entered(_body: Node2D):
+	if type == "health":
+		Globals.health += 10
+	elif type == "laser":
+		Globals.laser_amount += 5
+	elif type == "grenade":
+		Globals.grenade_amount += 1
+	else:
+		print("Unknown item type: " + type)
+	queue_free()
