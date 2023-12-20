98234587f8c7230c63213b8ec8dc48855608a3bf
DTNevaeha <DTNevaeha@gmail.com>
2023-12-20 17:19:07 -0600

    Added enemy attacks and update inside

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -9,6 +9,9 @@ var item_scene: PackedScene = preload("res://scenes/items/item.tscn")
 func _ready():
 	for container in get_tree().get_nodes_in_group("Container"):
 		container.connect("open", _on_container_opened)
+	
+	for scout in get_tree().get_nodes_in_group("Scouts"):
+		scout.connect("laser", _on_scout_laser)
 
 
 func _on_container_opened(pos, direction):
@@ -19,12 +22,19 @@ func _on_container_opened(pos, direction):
 
 
 func _on_player_laser(pos, direction):
+	create_laser(pos, direction)
+
+
+func _on_scout_laser(pos, direction):
+	create_laser(pos, direction)
+
+
+func create_laser(pos, direction):
 	var laser = laser_scene.instantiate() as Area2D
 	laser.position = pos
 	laser.rotation_degrees = rad_to_deg(direction.angle()) + 90
 	laser.direction = direction
 	$Projectiles.add_child(laser)
-	$UI.update_laser_text()
 
 
 func _on_player_grenade(pos, direction):
@@ -32,23 +42,3 @@ func _on_player_grenade(pos, direction):
 	grenade.position = pos
 	grenade.linear_velocity = direction * grenade.speed
 	$Projectiles.add_child(grenade)
-	$UI.update_grenade_text()
-
-
-func _on_house_player_entered():
-	var tween = get_tree().create_tween()
-	tween.set_parallel(true)
-	# tween.tween_property($Player, "modulate:a", 0, 2).from(0.5)
-	tween.tween_property($Player/Camera2D, "zoom", Vector2(1, 1), 1)
-
-
-func _on_house_player_exited():
-	var tween = get_tree().create_tween()
-	tween.set_parallel(true)
-	tween.tween_property($Player, "modulate:a", 1, 2)
-	tween.tween_property($Player/Camera2D, "zoom", Vector2(0.6, 0.6), 1)
-
-
-func _on_player_update_stats():
-	$UI.update_laser_text()
-	$UI.update_grenade_text()
