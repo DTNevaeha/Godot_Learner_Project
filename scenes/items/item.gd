8d14da7f068eeca500ded56f530c5c215834c1d7
DTNevaeha <DTNevaeha@gmail.com>
2023-12-19 19:16:30 -0600

    Crates and toilets now spawn items

--- scenes/items/item.gd
+++ scenes/items/item.gd
@@ -4,6 +4,9 @@ var rotation_speed: int = 4
 var avaliable_options = ["laser", "laser", "laser", "laser", "grenade", "health"]
 var type = avaliable_options[randi() % len(avaliable_options)]
 
+var direction: Vector2
+var distance: int = randi_range(150, 250)
+
 
 func _ready():
 	if type == "laser":
@@ -12,6 +15,13 @@ func _ready():
 		$Sprite2D.modulate = Color(0.8, 0.2, 0.1)
 	elif type == "health":
 		$Sprite2D.modulate = Color(0.1, 0.8, 0.1)
+
+	# tweens
+	var target_pos = position + direction * distance
+	var tween = create_tween()
+	tween.set_parallel(true)
+	tween.tween_property(self, "position", target_pos, 0.5)
+	tween.tween_property(self, "scale", Vector2(1, 1), 0.3).from(Vector2(0, 0))
 
 
 func _process(delta):
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -5,6 +5,7 @@ var can_grenade: bool = true
 
 signal laser(pos, direction)
 signal grenade(pos, direction)
+signal update_stats
 
 @export var max_speed: int = 500
 var speed: int = max_speed
@@ -32,11 +33,7 @@ func _process(_delta):
 		laser.emit(selected_laser.global_position, player_direction)
 
 	# Throw grenade
-	if (
-		Input.is_action_just_pressed("secondary action")
-		and can_grenade
-		and Globals.grenade_amount > 0
-	):
+	if Input.is_action_just_pressed("secondary action") and can_grenade and Globals.grenade_amount > 0:
 		Globals.grenade_amount -= 1
 		can_grenade = false
 		$GrenadeTimer.start()
@@ -50,3 +47,14 @@ func _on_laser_timer_timeout():
 
 func _on_grenade_timer_timeout():
 	can_grenade = true
+
+func add_item(type: String) -> void:
+	if type == "laser":
+		Globals.laser_amount += 5
+	elif type == "grenade":
+		Globals.grenade_amount += 1
+	elif type == "health":
+		Globals.health += 10
+	else:
+		print("Unknown item type: " + type)
+	update_stats.emit()
