e65a8b95da8fe204a93096070ee95b19076fa551
DTNevaeha <DTNevaeha@gmail.com>
2023-12-21 13:54:09 -0600

    Scout now flashes when hit

--- scenes/enemy/scout.gd
+++ scenes/enemy/scout.gd
@@ -15,6 +15,7 @@ func hit():
 		health -= 10
 		vulnerable = false
 		$Timers/HitTimer.start()
+		$Sprite2D.material.set_shader_parameter("progress", 1)
 	if health <= 0:
 		queue_free()
 
@@ -47,3 +48,4 @@ func _on_laser_timer_timeout():
 
 func _on_hit_timer_timeout():
 	vulnerable = true
+	$Sprite2D.material.set_shader_parameter("progress", 0)
