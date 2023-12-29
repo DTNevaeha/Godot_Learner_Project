aa65a64083c8713e69b0dc6064a327535b27d036
DTNevaeha <DTNevaeha@gmail.com>
2023-12-29 11:22:49 -0600

    Hunter can now be killed and fixed visuals

--- scenes/enemy/hunter.gd
+++ scenes/enemy/hunter.gd
@@ -4,6 +4,9 @@ var active: bool = false
 var player_nearby: bool = false
 var speed: int = 200
 
+var vulnerable: bool = true
+var health: int = 100
+
 
 func _ready():
 	$NavigationAgent2D.path_desired_distance = 4.0
@@ -20,6 +23,7 @@ func _physics_process(_delta):
 		var look_angle = direction.angle()
 		rotation = look_angle + PI / 2
 
+
 func _on_notice_area_body_entered(_body: Node2D):
 	active = true
 	$AnimationPlayer.play("walk")
@@ -27,6 +31,7 @@ func _on_notice_area_body_entered(_body: Node2D):
 
 func _on_notice_area_body_exited(_body: Node2D):
 	active = false
+	$AnimationPlayer.stop()
 
 
 func _on_navigation_timer_timeout():
@@ -34,15 +39,28 @@ func _on_navigation_timer_timeout():
 		$NavigationAgent2D.target_position = Globals.player_pos
 
 
-func _on_attack_area_body_entered(_body:Node2D):
+func _on_attack_area_body_entered(_body: Node2D):
 	player_nearby = true
 	$AnimationPlayer.play("attack")
 
 
-func _on_attack_area_body_exited(_body:Node2D):
+func _on_attack_area_body_exited(_body: Node2D):
 	player_nearby = false
+	$AnimationPlayer.play("walk")
 
 
 func attack():
 	if player_nearby:
 		Globals.health -= 20
+
+
+func hit():
+	if vulnerable:
+		health -= 10
+		$Timers/HitTimer.start()
+	if health <= 0:
+		queue_free()
+
+
+func _on_hit_timer_timeout():
+	vulnerable = true
