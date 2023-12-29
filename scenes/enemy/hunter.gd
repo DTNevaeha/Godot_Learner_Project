659e0fbd79059f3f0d33bbc23f0292fadb709a81
DTNevaeha <DTNevaeha@gmail.com>
2023-12-28 18:47:09 -0600

    Updated animations for hunter

--- scenes/enemy/hunter.gd
+++ scenes/enemy/hunter.gd
@@ -1,10 +1,13 @@
 extends CharacterBody2D
 
 var active: bool = false
+var player_nearby: bool = false
 var speed: int = 200
 
 
 func _ready():
+	$NavigationAgent2D.path_desired_distance = 4.0
+	$NavigationAgent2D.target_desired_distance = 4.0
 	$NavigationAgent2D.target_position = Globals.player_pos
 
 
@@ -14,11 +17,12 @@ func _physics_process(_delta):
 		var direction: Vector2 = (next_path_pos - global_position).normalized()
 		velocity = direction * speed
 		move_and_slide()
-		look_at(Globals.player_pos)
-
+		var look_angle = direction.angle()
+		rotation = look_angle + PI / 2
 
 func _on_notice_area_body_entered(_body: Node2D):
 	active = true
+	$AnimationPlayer.play("walk")
 
 
 func _on_notice_area_body_exited(_body: Node2D):
@@ -28,3 +32,16 @@ func _on_notice_area_body_exited(_body: Node2D):
 func _on_navigation_timer_timeout():
 	if active:
 		$NavigationAgent2D.target_position = Globals.player_pos
+
+
+func _on_attack_area_body_entered(_body:Node2D):
+	player_nearby = true
+	$AnimationPlayer.play("attack")
+
+
+func _on_attack_area_body_exited(_body:Node2D):
+	player_nearby = false
+
+
+func attack():
+	Globals.health -20
