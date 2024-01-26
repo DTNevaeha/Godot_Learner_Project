c7740f092426c1fa1db612f56c8a2ba5258bcdf3
DTNevaeha <DTNevaeha@gmail.com>
2024-01-25 20:57:25 -0600

    Moving_platform now pauses before moving again

--- scenes/traps/moving_platform.gd
+++ scenes/traps/moving_platform.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
-var speed: int = 200
-var distance: int = 500
+var speed: int = 300
 var move_up: bool = true
 var direction: Vector2 = Vector2.UP
+var distance: int = 500
+var moved_distance: float = 0.0
 
 
 func _process(delta):
+	if $Timer.is_stopped():
+		return
 	if move_up:
 		global_position += direction * speed * delta
 	else:
@@ -15,3 +18,17 @@ func _process(delta):
 
 func _on_timer_timeout():
 	move_up = not move_up
+	$Timer.stop()
+	await get_tree().create_timer(2.0).timeout
+	$Timer.start()
+	
+
+# func _physics_process(delta):
+# 	if move_up and moved_distance < distance:
+# 		var move_step = speed * delta
+# 		if moved_distance + move_step > distance:
+# 			move_step = distance - moved_distance
+# 		position.y -= move_step
+# 		moved_distance += move_step
+# 	elif move_up:
+# 		move_up = false
